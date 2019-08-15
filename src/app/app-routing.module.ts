f12ac10998f285c7e89b70a89885c9183e9eba14
trong.ngotrung@gmail.com <trong.ngotrung@gmail.com>
2019-08-16 00:19:43 +0700

    routing  and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/list', pathMatch: 'full' },
+  {path: 'list', component: EmployeeListComponent},
+  {path: 'create', component: CreateEmployeeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [EmployeeListComponent,
+                                  CreateEmployeeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
